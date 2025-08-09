f8c2bccc50de47d9e264cd0d7cd4987d2d22ac79
KaganTerzi <kagan.terzi34@gmail.com>
2025-08-10 01:11:59 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from './components/ThemeProvider'
+import { ThemeProvider } from '../components/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,4 +24,4 @@ export default function RootLayout({
       </body>
     </html>
   )
-} 
+}
